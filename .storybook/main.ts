5f9cc2cb3dcc2f10416dad81bfcd77659b74b2a0
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-01-24 21:19:51 +0100

    Add storybook addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,13 @@ import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
 module.exports = {
   stories: ['../app/lib/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-themes'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-addon-themes',
+    '@storybook/addon-viewport',
+    '@storybook/addon-a11y',
+  ],
   framework: '@storybook/react',
   webpackFinal: async (config) => {
     config.resolve.plugins = [
