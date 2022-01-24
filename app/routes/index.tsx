092d42f12b8b36f92e5a20e1314f3ff9a51a3134
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-01-24 22:27:57 +0100

    Second try

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,23 @@
+import { Button } from '~/lib/components/Button';
+import { Theme } from '~/lib/enums';
+
 const Index = () => (
-  <div className="w-screen h-screen flex flex-col gap-8 items-center justify-center bg-gray-200 text-gray-800 dark:bg-gray-800 dark:text-gray-200">
-    <h1 className="font-semibold text-5xl">Hello world</h1>
-    <h2 className="font-semibold text-3xl">Hello world</h2>
+  <div className="w-screen h-screen flex flex-col gap-4 items-center justify-center bg-gray-200 text-gray-800 dark:bg-gray-800 dark:text-gray-200">
+    <h1 className="font-semibold text-5xl">Remix CT8 template</h1>
+    <h2 className="text-xl">Includes:</h2>
+    <ul className="list-disc text-sm">
+      <li>ESLint and Prettier as code style enforcers</li>
+      <li>Jest with Testing Library for testing shared elements (with codecov config)</li>
+      <li>Tailwind for styling with dark theme config and handling</li>
+      <li>Storybook to design components in isolation</li>
+      <li>Express for serving Remix app</li>
+      <li>Workflow setup for automatic deployments and code checking</li>
+    </ul>
+    <div className="flex gap-4">
+      <Button onClick={() => window.setTheme(Theme.DARK)}>Dark mode</Button>
+      <Button onClick={() => window.setTheme(Theme.LIGHT)}>Light mode</Button>
+      <Button onClick={() => window.setTheme(Theme.SYSTEM)}>System preferences</Button>
+    </div>
   </div>
 );
 
